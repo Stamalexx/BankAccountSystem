7211764b1c717dd6b13fc190ed9c315485b5f6b3
Alex <stamelosalexios@gmail.com>
2025-07-18 08:16:01 +0000

    added super class CurrentAccount

---
+++ src/CurrentAccount.java
@@ -0,0 +1,14 @@
+public class CurrentAccount extends BankAccount {
+    public CurrentAccount(String accountNumer, double balance, String accountHolderName){
+        super(accountNumer, balance, accountHolderName);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        
+        
+    }
+    
+
+    
+}
